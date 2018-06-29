64f6c2eea9ecaddbfa0b63678c8c8a29f5a9e5dc
Luis Rodolfo <rodolfolarae@gmail.com>
2018-06-28 23:45:46 -0500

    Login 2 Commit

--- NodeServer/server/boot/root.js
+++ NodeServer/server/boot/root.js
@@ -4,8 +4,6 @@ module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   //router.get('/', server.loopback.status());
-  router.get('/', function(req,res){
-  	return res.send('hello')
-  });
+  router.get('/', server.loopback.status());
   server.use(router);
 };
